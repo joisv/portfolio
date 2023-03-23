babc471a1d3ebc8083343c4aac74ee903bed2534
joisv <joisvvv@hotmail.com>
2023-03-23 11:13:36 +0700

    memperberui tampilan home

--- src/components/maininfo.js
+++ src/components/maininfo.js
@@ -1,12 +1,21 @@
 export default function MainInfo(props){
     return (
         <>
-            <div className='text-primary md:text-[70px] text-[40px] font-druk mt-[30vh] sm:mt-[37vh] relative -z-50 mb-[35vh]'>
-                <h1 className='relative'>Fullstack</h1>
-                <p className='relative md:-top-9 -top-4'>web dev.</p>
-                {props.isMobile ? <p className='text-base text-slate-200 font-sans '>Highly skilled at progressive enhancement, design systems & UI Engineering</p> : ''}
+            <div className="fixed">
+                <div className='text-primary md:text-[70px] text-[40px] font-druk mt-[30vh] sm:mt-[30vh] relative -z-50 mb-[35vh] bg-yellow-400'>
+                    <h1 className='relative'>Fullstack</h1>
+                    <p className='relative md:-top-9 -top-4'>web dev.</p>
+                    {props.isMobile ? <p className='text-base text-slate-200 font-sans '>Highly skilled at progressive enhancement, design systems & UI Engineering</p> : ''}
+                </div>
             </div>
-            <div className='sm:flex block mb-20'>
+            <div className="w-full h-[110vh]">
+                <div className="door">
+                    <div className="door-content">
+                        <h1 className="text-slate-200 text-end text-8xl">Halo dunia</h1>
+                    </div>
+                </div>
+            </div>
+            {/* <div className='sm:flex block mb-20'>
                 <div className='text-violet-500 mb-10'>
                 <h2 className='text-2xl mb-5 sm:text-4xl font-druk'>Design</h2>
                 <p>I'm probably not the typical designer positioned behind an Illustrator artboard adjusting pixels, but I design. Immersed in stylesheets tweaking font sizes and contemplating layouts is where you'll find me (~_^). I'm committed to creating fluent user experiences while staying fashionable.</p>
@@ -15,7 +24,10 @@ export default function MainInfo(props){
                 <h2 className='text-2xl mb-5 sm:text-4xl font-druk'>Enginering</h2>
                 <p>In building PHP and JavaScript applications, I'm equipped with just the right tools, and can absolutely function independently of them to deliver fast, resilient solutions optimized for scale — performance and scalabilty are priorities on my radar.</p>
                 </div>
-            </div>
+            </div> */}
+           
         </>
     )
 }
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className='w-full h-fit flex flex-wrap md:justify-end justify-center px-10 sm:px-14 pb-5'>
-          <MainInfo isMobile={isMobile}/>
+        <div className='w-full min-h-screen flex gap-2 justify-between px-10 sm:px-14 pb-5'>
+          <div className="">
+                {/* <div className='text-primary md:text-[70px] text-[40px] font-druk mt-[30vh] sm:mt-[30vh] relative -z-50 mb-[35vh] bg-yellow-400'>
+                    <h1 className='relative'>Fullstack</h1>
+                    <p className='relative md:-top-9 -top-4'>web dev.</p>
+                    {isMobile ? <p className='text-base text-slate-200 font-sans '>Highly skilled at progressive enhancement, design systems & UI Engineering</p> : ''}
+                </div> */}
+            </div>
+            <div className="h-[110vh] w-[80vw]">
+                <div className="door sm:top-20 top-32 sm:h-[200px] h-[135px]">
+                    <div className="door-content text-primary">
+                        <h1 className="sm:text-8xl text-9xl font-druk font-semibold text-end ">MOVIE VUE</h1>
+                        <h1 className="sm:text-8xl text-9xl font-druk font-semibold text-end">COMIC APP</h1>
+                        <h1 className="sm:text-8xl text-9xl font-druk font-semibold text-end">WEATHER APP</h1>
+                    </div>
+                </div>
+                  
+            </div>
         </div>
       </main>
     </>
